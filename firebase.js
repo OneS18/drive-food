3b01d8f6451e583108d1ef69672eb9bca606c380
OneS18 <diablo201888@gmail.com>
2022-06-16 11:09:29 +0300

    add dotenv

--- firebase.js
+++ firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
-
+import dotenv from "dotenv";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBiQyU-yRvda_SZqyLYqJk4eu0rtHhexR0",
+  apiKey: process.env.API_KEY,
   authDomain: "food-driver-c9cfe.firebaseapp.com",
   projectId: "food-driver-c9cfe",
   storageBucket: "food-driver-c9cfe.appspot.com",
   messagingSenderId: "43282508522",
-  appId: "1:43282508522:web:f43b8ea5e645d7495e0593"
+  appId: "1:43282508522:web:f43b8ea5e645d7495e0593",
 };
 
 // Initialize Firebase
